6dc8a8ff6852e9dda3472e94501fb68aadaf76a1
EC2 Default User <ec2-user@ip-172-31-41-93.ap-northeast-1.compute.internal>
2024-05-26 09:07:13 +0000

    fix customer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,11 +2,11 @@ class Booking < ApplicationRecord
 
    belongs_to :customer
    belongs_to :room
-   has_many :tag_relationships, dependent: :destroy
-   has_many :tags, through: :tag_relationships
+   # has_many :tag_relationships, dependent: :destroy
+   # has_many :tags, through: :tag_relationships
 
   validates :booking_date, presence: true
   validates :time, presence: true
   validates :number_of_people, presence: true
 
-end  
+end
